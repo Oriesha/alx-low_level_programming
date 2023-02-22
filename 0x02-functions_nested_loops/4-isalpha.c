2e81cf7f1f302209040bc3d08a6930ee35294f34
Orifha <orifhaxandukani@gmail.com>
2023-02-22 08:38:30 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,16 @@
 
 
 /**
- * _islower - Checks for lowercase character
- * @c: The character to be checked
- * Return: 1 for lowrcase character or 0 for anthing else
- */
+* _islower - Checks for alphabetic character
+* @c: The character to be checked
+* Return: 1 for alphabetic  character or 0 for anthing else
+*/
 
 int _isalpha(int c)
-
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
-        return (0);
+	return (0);
 }
