662f1159da89481408ff6af87d50c038d12ca2ab
Orifha <orifhaxandukani@gmail.com>
2023-03-10 09:00:59 -0800

    task

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+
+{
+
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t-");
+	_puts("Kevin Mitnick"); /*Print the name of the hacker on a new line*/
+	return (0);
+}
